3197419c76c8b9aa840d3c526d6942f6ef0ee10c
dongchancho <deamp@naver.com>
2023-12-25 21:25:30 +0900

    리액트의 상태 업데이트 처리방식 두가지

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,11 +1,40 @@
+import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () =>{
+    // const [enteredTitle,setEnteredTitle] = useState('');
+    // const [enteredAmount,setEnteredAmount] = useState('');
+    // const [enteredDate,setEnteredDate] = useState('');
+    
+    //상태를 하나로 선언하기
+    const [userInput,setUserInput]= useState({
+        enteredTitle :'',
+        enteredAmount :'',
+        enteredDate :''
+    });
+
     // 바닐라 Js
     // document.getElementById('').addEventListener('click',(event)=>{})
+    
     const titleChangeHandler = (event)=>{
-        console.log(event.target.value);
+        // setEnteredTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredTitle : event.target.value;
+        })
+    }
+    const amountChangeHandler = (event)=>{
+        setUserInput({
+            ...userInput,
+            enteredAmount : event.target.value;
+        })
+    }
+    const dateChangeHandler = (event)=>{
+        setUserInput({
+            ...userInput,
+            enteredDate : event.target.value;
+        })
     }
 
     return (
@@ -18,14 +47,14 @@ const ExpenseForm = () =>{
             </div>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
-                    <label>Amout</label>
-                    <input type="number" min="0.01" step="0.01"/>
+                    <label>Amount</label>
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
                 </div>
             </div>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2023-12-31"/>
+                    <input type="date" min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className='new-expense__actions'>
