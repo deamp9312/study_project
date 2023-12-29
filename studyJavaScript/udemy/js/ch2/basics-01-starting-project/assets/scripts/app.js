b26d650dd3a0873ad223030676e86e73b004e471
dongchancho <deamp@naver.com>
2023-12-29 23:39:13 +0900

    js공부 다시2

--- studyJavaScript/udemy/js/ch2/basics-01-starting-project/assets/scripts/app.js
+++ studyJavaScript/udemy/js/ch2/basics-01-starting-project/assets/scripts/app.js
@@ -1,2 +1,31 @@
+// const userInput = document.getElementById('input-number');
+// const addBtn = document.getElementById('btn-add');
+// const subtractBtn = document.getElementById('btn-subtract');
+// const multiplyBtn = document.getElementById('btn-multiply');
+// const divideBtn = document.getElementById('btn-divide');
 
-alert('hi');
+// const currentResultOutput = document.getElementById('current-result');
+// const currentCalculationOutput = document.getElementById('current-calculation');
+
+// function outputResult(result, text) {
+//   currentResultOutput.textContent = result;
+//   currentCalculationOutput.textContent = text;
+// }
+
+
+let defaultResult = 0 ;
+let currentResult = defaultResult;
+
+function add(num1,num2){
+    const result = num1+num2;
+    alert('result is ' + result);
+}
+
+let calculationDescription = `hihi ${currentResult}`;
+
+currentResult = currentResult + 10;
+let errorMessage = 'An error \n' + 'occurred!';
+//wite-space : pre로 설정하면 줄바꿈을 화면상에서 볼수있음.
+
+
+outputResult(currentResult,errorMessage)
