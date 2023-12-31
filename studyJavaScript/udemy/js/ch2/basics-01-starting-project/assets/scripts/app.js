cae901fb34d40eec74b82a7c83de3445d34044dd
dongchancho <deamp@naver.com>
2023-12-31 22:50:54 +0900

    js공부 2023년 마지막

--- studyJavaScript/udemy/js/ch2/basics-01-starting-project/assets/scripts/app.js
+++ studyJavaScript/udemy/js/ch2/basics-01-starting-project/assets/scripts/app.js
@@ -1,43 +1,51 @@
-// const userInput = document.getElementById('input-number');
-// const addBtn = document.getElementById('btn-add');
-// const subtractBtn = document.getElementById('btn-subtract');
-// const multiplyBtn = document.getElementById('btn-multiply');
-// const divideBtn = document.getElementById('btn-divide');
-
-// const currentResultOutput = document.getElementById('current-result');
-// const currentCalculationOutput = document.getElementById('current-calculation');
-
-// function outputResult(result, text) {
-//   currentResultOutput.textContent = result;
-//   currentCalculationOutput.textContent = text;
-// }
-
-
 let defaultResult = 0 ;
 let currentResult = defaultResult;
-
-function add(num1,num2){
-    const result = num1+num2;
-    return result;
-}
-
 let calculationDescription = `hihi ${currentResult}`;
+let logEntries =[];
 
-currentResult = currentResult + 10;
-let errorMessage = 'An error \n' + 'occurred!';
-//wite-space : pre로 설정하면 줄바꿈을 화면상에서 볼수있음.
-
-
-outputResult(currentResult,errorMessage);
-
-
-
-// test
-
-let userName = 'Max';
-function greetUser(name) {
-  let userName = name;
-  alert(userName);
+const getUserNumberInput = () =>{
+    return parseInt(userInput.value);
 }
-userName = 'Manu';
-greetUser(userName);
+
+const createAndWriteOutput = (operator,resultBeforeCalc,calcNumber) =>{
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult,calcDescription);
+}
+
+function add(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteOutput('+',initialResult,enteredNumber);
+    logEntries.push(enteredNumber);
+    console.log(logEntries);
+}
+const subtract = () =>{
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-',initialResult,enteredNumber);
+}
+
+const multiply = () =>{
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*',initialResult,enteredNumber);
+}
+
+const divide = () =>{
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /=  enteredNumber;
+    createAndWriteOutput('/',initialResult,enteredNumber);
+}
+// currentResult = currentResult + 10;
+let errorMessage = 'An error \n' + 'occurred!';
+//wite-space : pre 로 설정하면 줄바꿈을 화면상에서 볼수있음.
+
+
+addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiply);
+divideBtn.addEventListener('click',divide);
