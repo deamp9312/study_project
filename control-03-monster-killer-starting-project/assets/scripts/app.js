d16421e52f827dbd12ce4cc55e9f4797c6baca03
dongchancho <deamp@naver.com>
2024-01-03 23:12:34 +0900

    test

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,11 +1,36 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
+
 let choseMaxLife = 100;
 let currentMonsterHealth = choseMaxLife;
 let currentPlayerHealth = choseMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(choseMaxLife);
+
+function endRound(){
+    const initialPlayerHealth = currentPlayerHealth;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+
+    if(currentPlayerHealth <= 0 && hasBonusLife){
+        hasBonusLife =false;
+        removeBonusLife();
+        currentPlayerHealth =initialPlayerHealth;
+        alert('보너스 라이프를 사용하였습니다.');
+        setPlayerHealth(initialPlayerHealth);
+    }
+
+    if(currentMonsterHealth<=0 && currentPlayerHealth >0){
+        alert('You won!');
+    }else if(currentPlayerHealth<=0 && currentMonsterHealth>0){
+        alert('You lost!');
+    }else if(currentMonsterHealth<=0 && currentPlayerHealth<=0){
+        alert('You have a draw!');
+    }
+}
 
 function attackMonster(mode){
     let maxDamage;
@@ -16,15 +41,7 @@ function attackMonster(mode){
     }
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-    currentPlayerHealth -= playerDamage;
-    if(currentMonsterHealth<=0 && currentPlayerHealth >0){
-        alert('You won!');
-    }else if(currentPlayerHealth<=0 && currentMonsterHealth>0){
-        alert('You lost!');
-    }else if(currentMonsterHealth<=0 && currentPlayerHealth<=0){
-        alert('You have a draw!');
-    }
+    endRound();
 }
 
 function attackHandler(){
@@ -34,6 +51,21 @@ function attackHandler(){
 function strongAttackHandler(){
     attackMonster('STRONG_ATTACK');
 }
+//변수 쉐도윙? 이게 뭐지
+function healPlayerHandler() {
+    let healValue;
+    if(currentPlayerHealth>=choseMaxLife-HEAL_VALUE){
+        currentPlayerHealth=choseMaxLife;
+        alert("you can't heal to more than your max initial health.");
+        healValue=choseMaxLife-currentPlayerHealth;
+    }else{
+        healValue = HEAL_VALUE;
+    }
+    increasePlayerHealth(healValue);
+    currentPlayerHealth += healValue;
+    endRound();
+}
 
 attackBtn.addEventListener('click',attackHandler);
 strongAttackBtn.addEventListener('click',strongAttackHandler);
+healBtn.addEventListener('click',healPlayerHandler);
