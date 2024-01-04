58ed7e23f6a9c3983f88e3fe54724c9ecf50ea5e
dongchancho <deamp@naver.com>
2024-01-04 22:34:51 +0900

    호이스팅 메모

--- control-03-monster-killer-starting-project/assets/scripts/vendor.js
+++ control-03-monster-killer-starting-project/assets/scripts/vendor.js
@@ -42,3 +42,20 @@ function removeBonusLife() {
 function setPlayerHealth(health) {
   playerHealthBar.value = health;
 }
+
+/**
+ * 호이스팅 -> 자바스크립트는 실행하면 전체 스크립트를 확인해서 함수를 찾은 뒤 자로등으로 로드하고
+ * 등록해서 실제 사용하는 코드 아래에 함수를 작성하도록 하는 것(let,const,var)
+ * 
+ * 따라서 아래의 예시는 실행시점에 변경됨, 그러나 let,const는 값을 아예 할당하지않아 오류가 발생
+ * 하지만 var은 undefined로 값을 설정해줘서 로그 찍을때 undefined값이 찍히게 되는거
+ * 
+ * ------예시가 실행시점에 변경되는거 ---
+ * var userName;
+ * console.log(userName);
+ * userName = "cho";
+ * ------예시
+ * console.log(userName);
+ * var userName = "cho";
+ * -------
+ */
