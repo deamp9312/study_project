297536d2f7d57cb48ddda5e155ec48a6c512aa8b
dongchancho <deamp@naver.com>
2024-01-14 22:21:27 +0900

    Jpa재공부하기위해 프로젝트 기본설정 커밋

---
+++ restudyjpa/src/main/java/com/deamp/restudyjpa/RestudyjpaApplication.java
@@ -0,0 +1,13 @@
+package com.deamp.restudyjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestudyjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestudyjpaApplication.class, args);
+	}
+
+}
---
+++ restudyjpa/src/test/java/com/deamp/restudyjpa/RestudyjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deamp.restudyjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestudyjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
