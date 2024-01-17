4040fa1dfcdc90f9595d8854f75274dc334c184a
dongchancho <deamp@naver.com>
2024-01-17 23:17:47 +0900

    default entity settings

--- restudyjpa/src/test/java/com/deamp/restudyjpa/RestudyjpaApplicationTests.java
+++ restudyjpa/src/test/java/com/deamp/restudyjpa/RestudyjpaApplicationTests.java
@@ -9,10 +9,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Commit
 class RestudyjpaApplicationTests {
 
 	@Autowired
@@ -24,14 +28,14 @@ class RestudyjpaApplicationTests {
 		Hello hello = new Hello();
 		em.persist(hello);
 		JPAQueryFactory query = new JPAQueryFactory(em);
-		QHello qHello = new QHello("h");
+		QHello qHello = QHello.hello;
 
 		Hello result = query
 				.selectFrom(qHello)
 				.fetchOne();
 
-		Assertions.assertThat(result).isEqualTo(hello);
-
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
