2065ffa2939b9b79bc0eab43c52e8d7dbea10661
dongchancho <deamp@naver.com>
2024-01-17 22:23:57 +0900

    query spring boot 3.x default Setting_망할 설정 너무 싫다..

--- restudyjpa/src/test/java/com/deamp/restudyjpa/RestudyjpaApplicationTests.java
+++ restudyjpa/src/test/java/com/deamp/restudyjpa/RestudyjpaApplicationTests.java
@@ -1,13 +1,36 @@
 package com.deamp.restudyjpa;
 
+import com.deamp.restudyjpa.entity.Hello;
+import com.deamp.restudyjpa.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class RestudyjpaApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+
 	}
 
 }
+
