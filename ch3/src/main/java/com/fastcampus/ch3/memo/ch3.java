e6d0997ad962619a0e523037bbfdfbc14d50a193
dongchancho <deamp@naver.com>
2024-01-28 16:29:54 +0900

    autowired 와 resource 설명

---
+++ ch3/src/main/java/com/fastcampus/ch3/memo/ch3.java
@@ -0,0 +1,17 @@
+package com.fastcampus.ch3.memo;
+
+
+public class ch3 {
+
+    /**
+     * OOP
+     * -분리(AOP) :  1 . 변하는것과 변하지 않는것을 분리
+     *              2 . 관심사 분리
+     *              3 . 중복코드 분리
+     */
+
+    /**
+     * Properties를 사용하는 이유 > map과 비슷한 역활인데
+     * load 라는 메소드가 파일에서 데이터를 읽어오기 편리하게 되어있음
+     */
+}
