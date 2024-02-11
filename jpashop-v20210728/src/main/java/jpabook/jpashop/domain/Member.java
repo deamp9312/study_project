b157f3cd3af26c117aa02336854131578bb3b691
dongchancho <deamp@naver.com>
2024-02-11 17:14:18 +0900

    studyJpa

---
+++ jpashop-v20210728/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
---
+++ jpashop-v20210728/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jpabook.jpashop.domain.item.Book;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JpashopApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Test
+	public void testCreateUser() throws Exception {
+		// CreateUserRequest 객체를 생성합니다.
+		Book newBook = new Book();
+		newBook.setName("cho");
+		newBook.setPrice(1000);
+		newBook.setStockQuantity(10);
+		newBook.setAuthor("조동찬");
+		newBook.setIsbn("1");
+		// "/api/users/create"에 대한 POST 요청 모의
+		mockMvc.perform(MockMvcRequestBuilders.post("/items/new")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(newBook)))
+				// HTTP 상태코드 200인지 확인
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				// Content-Type이 "application/json"인지 확인
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+				// JSON 응답에서 필드 값 확인
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("cho"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.price").value(1000));
+	}
+
+}
