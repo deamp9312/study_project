73b67891536bcb9bacf55333dd437da89aeaee07
dongchancho <deamp@naver.com>
2024-02-14 23:04:45 +0900

    운영환경에 따라 Yml 설정하는법

---
+++ jpashop-v20210728/src/main/java/jpabook/jpashop/AppConfig.java
@@ -0,0 +1,9 @@
+package jpabook.jpashop;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+//@Configuration
+//@PropertySource("classpath:application-${spring.profiles.active}.yml")
+//public class AppConfig {
+//}
