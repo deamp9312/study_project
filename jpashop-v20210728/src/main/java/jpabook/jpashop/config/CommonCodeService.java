12505aded37ae5135e91db89ac7e0be013ec49cc
dongchancho <deamp@naver.com>
2024-05-11 14:36:29 +0900

    yml에 scheduled 주기를 설정해두고 사용하는방법

--- jpashop-v20210728/src/main/java/jpabook/jpashop/config/CommonCodeService.java
+++ jpashop-v20210728/src/main/java/jpabook/jpashop/config/CommonCodeService.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.config;
 import jpabook.jpashop.dto.Code;
 import jpabook.jpashop.dto.CodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +14,14 @@ import java.util.List;
 @Component
 public class CommonCodeService {
 
+
     @Autowired
     private CodeRepository repository;
 
     private List<Code> codes = new ArrayList<>(); // 메모리에 저장될 공통 코드 리스트
 
     // 10분마다 실행되는 메서드로, 데이터베이스에서 공통 코드를 조회하여 메모리에 저장합니다.
-//    @Scheduled(fixedRate = 30000) // 10분마다 실행 (1000ms * 60초 * 10분)
+    @Scheduled(fixedDelayString = "${common.code.time}") // 10분마다 실행 (1000ms * 60초 * 10분)
     public void refreshCommonCodes() {
         System.out.println("10분마다 실행됨");
         codes = repository.findAll();
